ed483e74949e94b9bb5bdcdd2d64d1a2471f0465
fhildeb <felix@lukso.io>
2022-05-21 01:01:10 +0200

    add telebook

---
+++ 23-telebook/README.md
@@ -0,0 +1,8 @@
+# Telefonbuch (Main)
+
+## Terminal
+
+```bash
+{heinz=[0174], Otto=[1234, 0174]}
+[1234, 0174]
+```
---
+++ 23-telebook/Telefonbuch.java
@@ -0,0 +1,71 @@
+package telebook;
+
+import java.util.*;
+
+public class Telefonbuch {
+
+    Map<String,Set<String>> Speicher;         //erster wert string, folgende werte eine samlung an strings 
+    
+    public static void main(String[] args) 
+    {
+        Telefonbuch t = new Telefonbuch();
+        t.einfuegen("Otto", "1234");
+        t.einfuegen("Otto", "0174");
+        t.einfuegen("heinz", "0174");
+        System.out.println(t);
+        System.out.println(t.HatTelNr("Otto"));
+    }
+    public Telefonbuch()
+    {
+        Speicher = new HashMap<String,Set<String>>();
+    }
+    public boolean einfuegen(String name,String nummer)
+    {
+        if(Speicher.containsKey(name))      
+        {       //name bekannt ,nummer gespeichert
+            Speicher.get(name).add(nummer);
+            return true;
+        }
+        else
+        {       //neuer name
+            HashSet<String> nummern = new HashSet<String>();
+            nummern.add(nummer);
+            Speicher.put(name, nummern);
+            return false;
+        }
+        
+    }
+    public String toString()
+    {
+        return Speicher.toString();
+    }
+    public String HatTelNr(String name)
+    {
+        if(Speicher.containsKey(name))
+        {
+            return Speicher.get(name).toString();
+        }
+        return "Fehler";
+    }
+    public boolean loeschen (String name)
+    {
+        if(Speicher.containsKey(name))
+        {
+            Speicher.remove(name);
+            return true;
+        }
+        else
+            return false;
+    }
+    public boolean loeschen(String name, String nummer)
+    {
+        if(Speicher.containsKey(name))
+        {
+            Speicher.get(name).remove(nummer);
+            return true;
+        }
+        else
+            return false;
+    }
+    
+}
