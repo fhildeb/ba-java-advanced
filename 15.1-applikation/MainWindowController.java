e81bf576db269a52b3341be845e62942155e9c1f
fhildeb <felix@lukso.io>
2022-05-20 22:42:31 +0200

    add applikation

---
+++ 15.1-applikation/MainWindowController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+
+public class MainWindowController {
+
+	@FXML public Label praktikumLabel;
+	@FXML public TextField textBox;
+	
+	
+	@FXML
+	public void handleCangeText(ActionEvent e) {
+		praktikumLabel.setText(textBox.getText());
+	}
+	
+	public void handleClearText(ActionEvent e) {
+		praktikumLabel.setText(null);
+	}
+}
---
+++ 15.1-applikation/README.md
@@ -0,0 +1,6 @@
+# Main (Main)
+
+## GUI
+
+![Main Vorher](./../img/15.1-applikation_01.png)
+![Main Nachher](./../img/15.1-applikation_01.png)
